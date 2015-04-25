d9590ecdbac764ecfef9bfad334b7721f40aae8c
Th80689 <thomashelmker@web.de>
2015-04-25 17:03:10 +0200

    Add sources for prog_assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall goal: 
+## Avoid unnecessary computations by caching matrix 
+## solving results for a given matrix object and check
+## for already existing results before starting calculation 
 
-## Write a short comment describing this function
-
-## here starts my code
+## Part 1: 
+## a) initialize comparison objects from function call (parent environment)
+## and from cache (current enviromnent)
+## b) define methods of this functions
+##   get: display input 
+##   setInverse: update cache
+##   getInverse: get cached result)  
+## c) define output as a list 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## initialize comparison objects
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Part 2:
+## check for given matrix x whether cache result exists
+## if yes: display cache info and return cache results
+## if no:  calculate matrix inverse and cache it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
